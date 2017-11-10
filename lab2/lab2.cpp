7b5c8fd7f31818cf432168b78253b5e30f93f2a3
Lev Bazylskyi <chozen.lev@gmail.com>
2017-11-10 11:32:08 +0200

    updated lab2: added compare table

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -23,34 +23,39 @@ double phi(double x, double lambda)
     return df(x) > 0.0 ? x - lambda*f(x) : x + lambda*f(x);
 }
 
-double iterativeMethod(double a, double b, double eps, double &delta)
+double iterativeMethod(double a, double b, double eps, double &delta, unsigned int &iter)
 {
     double m1 = fabs(df(a)), M1 = fabs(df(b));
     if (m1 > M1) {
         std::swap(m1, M1);
     }
     double x0, xk = (a+b)/2, lambda = 1.0/M1, q = 1.0 - m1/M1;
+    iter = 0;
 
     do
     {
         x0 = xk;
         xk = phi(x0, lambda);
+        iter++;
     } while (fabs(xk-x0) > (1-q)/q*eps);
     delta = fabs(xk-x0);
 
     return xk;
 }
 
-double tangentsMethod(double a, double b, double eps, double &delta)
+double tangentsMethod(double a, double b, double eps, double &delta, unsigned int &iter)
 {
     double m1 = fabs(df(a)), M1 = fabs(df(b));
     if (m1 > M1) {
         std::swap(m1, M1);
     }
     double xk = a;
+    iter = 0;
+
     do
     {
         xk = xk - f(xk)/df(xk);
+        iter++;
     } while (fabs(f(xk))/m1 > eps);
     delta = fabs(f(xk))/m1;
 
@@ -59,23 +64,38 @@ double tangentsMethod(double a, double b, double eps, double &delta)
 
 void printTableIterative(double a, double b)
 {
-    double x, delta = 0.0;
+    double x, delta;
+    unsigned int iterations;
     printf("[%.1f;%.1f], iterative\neps\tX\t\t\tdelta\n", a, b);
     for (double eps = 1e-2; eps > 1e-14; eps *= 1e-3)
     {
-        x = iterativeMethod(a, b, eps, delta);
+        x = iterativeMethod(a, b, eps, delta, iterations);
         printf("%.0e\t%.14f\t%.6e\n", eps, x, delta);
     }
 }
 
 void printTableTangents(double a, double b)
 {
-    double x, delta = 0.0;
+    double x, delta;
+    unsigned int iterations;
     printf("[%.1f;%.1f], tangents\neps\tX\t\t\tdelta\n", a, b);
     for (double eps = 1e-2; eps > 1e-14; eps *= 1e-3)
     {
-        x = tangentsMethod(a, b, eps, delta);
+        x = tangentsMethod(a, b, eps, delta, iterations);
         printf("%.0e\t%.14f\t%.6e\n", eps, x, delta);
+    }
+}
+
+void printTableCompare(double a, double b)
+{
+    double delta;
+    unsigned int iterations1, iterations2;
+    printf("[%.1f;%.1f], compare\neps\tIter\tTangents\n", a, b);
+    for (double eps = 1e-2; eps > 1e-14; eps *= 1e-3)
+    {
+        iterativeMethod(a, b, eps, delta, iterations1);
+        tangentsMethod(a, b, eps, delta, iterations2);
+        printf("%.0e\t%d\t%d\n", eps, iterations1, iterations2);
     }
 }
 
@@ -88,6 +108,8 @@ int main()
     printTableTangents(3.2, 3.7); printf("\n");
     printTableTangents(6.2, 6.7); printf("\n");
     printTableTangents(8.2, 8.7); printf("\n");
+
+    printTableCompare(3.2, 3.7); printf("\n");
 
     return 0;
 }
